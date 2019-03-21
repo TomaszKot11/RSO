df5a4f633904670ff0c296a3c5a19fb759ab5aa8
TomKo1 <kottomasz98@gmail.com>
2019-03-21 14:09:41 +0100

    Update

--- client.c
+++ client.c
@@ -8,22 +8,31 @@
 #include <unistd.h>
 #include <endian.h>   
 #include <byteswap.h>
+#include <pthread.h>
 
 
-//TODO: request_id! 
-// TODO: checing req_id
-// TODO: checking response code ;)
+//TODO: when 3 is clicked the program stops!
+// TODO: fix response code!
 
 void print_menu();
 void perform_square_root_request(int);
 void perform_date_request(int);
 void proceed_root_answer(int);
 void proceed_date_answer(int);
+// function that is run by the thread
+void* incomming_request_handler(void*);
+
+// this variables are modified only by one thread
+// that's why there are thread-safe
+int root_request_counter = 0;
+int date_request_counter = 0;
+int should_run = 1;
+int sockfd;
 
 
 int main () {
-   int should_run = 1;
-   int sockfd;
+   
+   
    socklen_t len;
    struct sockaddr_in address;
    int result;
@@ -40,7 +49,15 @@ int main () {
        perror ("Error while connectiong!");
        exit (1);
    }
-	
+
+	// run incomming request hanlder
+	pthread_t thread_id;
+	int error = pthread_create(&thread_id, NULL, incomming_request_handler, NULL);
+	if(error != 0) {
+		printf("\nThread can;t be created!\n");
+		exit(1);
+	}
+
 	while(should_run) {
 	        int request_no = 0;
 		do {	    
@@ -52,66 +69,79 @@ int main () {
 
 			switch(request_no) {
 			  case 1:
-				perform_date_request(sockfd);
-				break;
-			  case 2:
 				perform_square_root_request(sockfd);
 				break;
+			  case 2:
+				perform_date_request(sockfd);
+				break;
 			  case 3:
+				printf("Bye!");
 				should_run = 0;
-			       break;
+			       	break;
 			  default:
-				{
-					printf("No such option!");
-					printf("Inside the loop");
-				}
-				
+				printf("No such option!");
 			}
 		} while(request_no < 1 && request_no > 3);
-		//TODO: check req_id
-		//TODO: make separte thread to send the request
-	printf("outside the loop!");
-	uint32_t answer_code, request_id;
-	uint32_t h_answer_code, h_request_id;
-        // decode the request_code
-	ssize_t no_bytes = read(sockfd, &answer_code, sizeof(uint32_t));
-	if(no_bytes < 0) {
-		printf("[ANSWER_CODE] Wrong number of bytes were read!");
-		exit(1);	
+		
+
 	}
+   pthread_join(thread_id, NULL);	
+   close (sockfd);
+   printf("BYE!");
+   exit (0);
+}
 
-	h_answer_code = ntohl(answer_code);
 
-	no_bytes = read(sockfd, &request_id, sizeof(uint32_t));
-	if(no_bytes < 0) {
-		printf("[REQUEST_ID] Wrong number of bytes were read!");
-		exit(1);	
-	}
 
-	// decode the reuqest_id
-	h_request_id = ntohl(request_id);
+void* incomming_request_handler(void* vargp) {
+	//printf("Thread receiving responses started!");
+	while(should_run) {
+		uint32_t answer_code, request_id;
+		uint32_t h_answer_code, h_request_id;
+        	// decode the request_code
+		ssize_t no_bytes = read(sockfd, &answer_code, sizeof(uint32_t));
+		if(no_bytes < 0) {
+			printf("[ANSWER_CODE] Wrong number of bytes were read!");
+			exit(1);	
+		}
+
+		h_answer_code = ntohl(answer_code);
+
+		no_bytes = read(sockfd, &request_id, sizeof(uint32_t));
+		if(no_bytes < 0) {
+			printf("[REQUEST_ID] Wrong number of bytes were read!");
+			exit(1);	
+		}
+	
+		// decode the reuqest_id
+		h_request_id = ntohl(request_id);
 
 
 	
-	switch(h_answer_code) {
-		case 0xFF0000FFul:
-			// root answer 
-			proceed_root_answer(sockfd);			
-			break;
-		case 0xFF000002ul:
-			// date answer 
-			proceed_date_answer(sockfd);
-			break;
-		default:
-			printf("Wrong answer code!");
-			exit(1);
+		switch(h_answer_code) {
+			case 0xFF0000FFul:
+				// root answer 
+				if(root_request_counter < h_request_id) {
+					printf("Wrong request id!");
+					exit(1);
+				}
+				proceed_root_answer(sockfd);			
+				break;
+			case 0xFF000002ul:
+				// date answer 
+				if(date_request_counter < h_request_id) {
+					printf("Wrong request id!");
+					exit(1);
+				}
+				proceed_date_answer(sockfd);
+				break;
+			default:
+				printf("Wrong answer code!");
+				exit(1);
+		}
+
 	}
-
-
-
-	}	
-   close (sockfd);
-   exit (0);
+	//printf("Thread receiving responses ended!");
 }
 
 
@@ -129,7 +159,6 @@ void proceed_date_answer(int sockfd) {
 	uint32_t req_code;
 	uint32_t req_id;
 	uint32_t data_length;
-
 
 	ssize_t no_bytes = read(sockfd, &data_length, sizeof(uint32_t));
 	if(no_bytes < 0) {
@@ -199,14 +228,14 @@ void perform_square_root_request(int sockfd) {
 
 	memcpy(request_coded, &n_request_code, sizeof(n_request_code));
 	memcpy(request_coded + sizeof(n_request_code), &n_request_id , sizeof(n_request_id));
-	memcpy(request_coded + (sizeof(n_request_code) + sizeof(n_request_id)), &n_my_digit_bits, sizeof(n_my_digit_bits));
+	memcpy(request_coded + (sizeof(n_request_code) + sizeof(n_request_id)), &n_my_digit_bits, 		sizeof(n_my_digit_bits));
 
 	ssize_t no_bytes = write(sockfd, request_coded, 16 * sizeof(char));
 	if(no_bytes == -1) {
 		printf("Error while writing data!");
 		exit(1);
 	}
-
+	root_request_counter++;
 }
 
 
@@ -229,23 +258,7 @@ void perform_date_request(int sockfd) {
 	}
 
 
-	// read the response!
-
-	// read first 12 bytes to get the legth of the string
-	
-
-/*	no_bytes = read(sockfd, &req_code, sizeof(uint32_t));*/
-/*	if(no_bytes < 0) {*/
-/*		printf("[PROCEED_DATE_REQUEST] Error while reading data!");*/
-/*		exit(1);*/
-/*	}*/
-/*	*/
-/*	no_bytes = read(sockfd, &req_id, sizeof(uint32_t));*/
-/*	if(no_bytes < 0) {*/
-/*		printf("[PROCEED_DATE_REQUEST] Error while reading data!");*/
-/*		exit(1);*/
-/*	}*/
-	
+	date_request_counter++;
 }	
 
 
--- killer.sh
+++ killer.sh
@@ -3,9 +3,9 @@ if [[ $1 == "-k" ]] ; then
 	echo 'Socket closing!'
 	sudo fuser -k -n tcp 9734
 	pkill -9 server
-else 
+else
 	echo 'Compiling!'
 	gcc server.c -o server -lm
-	gcc client.c -o client
+	gcc client.c -o client -lpthread
 fi
 echo 'All operations performed'
